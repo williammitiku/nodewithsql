359891a57f6c8ff02efb200505a2ca5275df2c73
WilliamMitiku <58324632+williammitiku@users.noreply.github.com>
2023-12-12 11:05:30 +0300

    undo

--- mongo.js
+++ mongo.js
@@ -61,53 +61,13 @@ app.get('/sales2/products-summary', async (req, res) => {
   }
 });
 
-async function sendSMSFunction(message) {
-  const apiUrl = 'https://hahu.io/api/send/sms'; // Replace with your SMS service API URL
-  const apiKey = 'YOUR_API_KEY'; // Replace with your SMS service API key
-  
-  const smsData = {
-    secret: apiKey,
-    mode: 'devices',
-    phone: '+251921951592', // Replace with the recipient's phone number
-    message: message,
-    device: '00000000-0000-0000-aee4-1ad38bf6221e', // Replace with device ID
-    sim: 1,
-    priority: 1
-  };
-
-  // Sending the SMS using fetch or any HTTP client library
-  const response = await fetch(apiUrl, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(smsData),
-  });
-
-  const smsResult = await response.json();
-  return smsResult;
-}
-
 app.get('/salesNew', async (req, res) => {
   const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
   try {
     await client.connect();
     const db = client.db(dbName);
-    
-    // Assume you have payment details associated with the sale
-    const paymentDetails = 'Payment: $100'; // Replace with your payment details logic
-    
-    // Insert new sale record into the 'sales2' collection
-    const newSale = { /* Define your new sale object here */ };
-    await db.collection('sales2').insertOne(newSale);
-    
-    // Send thank-you message with payment details
-    const message = `Thank you for Doing Business with Us. ${paymentDetails}`;
-    
-    // Send SMS using the hypothetical function
-    const smsResult = await sendSMSFunction(message);
-    
-    res.json({ message: 'Sale recorded. Thank-you message sent.' });
+    const sales = await db.collection('sales2').find().toArray();
+    res.json(sales);
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Internal server error' });
